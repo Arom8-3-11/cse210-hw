625483308ed86745dc30253a3f940fd42a7158af
Arom8-3-11 <rom24003@byui.edu>
2024-09-24 22:15:04 -0600

    start

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        // write a new entry
+        Console.WriteLine("1.) New Entry");
+
+        // display the journal
+        Console.WriteLine("2.) Display Journal");
+
+        // save the journal to a file
+        Console.WriteLine("3.) Save to file")
+
+        // load the journal from a file
+        Console.WriteLine("4.) Load file")
+
+        // quit
+        Console.WriteLine("5.) Quit")
+
+
+
+
+
+        // Who was the most interesting person I interacted with today?
+
+        // What was the best part of my day?
+
+        // How did I see the hand of the Lord in my life today?
+
+        // What was the strongest emotion I felt today?
+
+        // If I had one thing I could do over today, what would it be?
+
+        // What is something that I would like to do in the future?
+
+        // What is something that I need to vent about?
+
+
     }
 }
