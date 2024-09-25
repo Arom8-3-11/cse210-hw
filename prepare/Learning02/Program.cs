1cc3fa368a8c7f2fc1b26c494089f0d344a30a9f
Arom8-3-11 <rom24003@byui.edu>
2024-09-24 20:49:24 -0600

    fixing bugs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2010;
+        job1._endYear = 2024;
+
+        //create second job. these two sets are what is going to be somehow displayed using the job.cs file
+        Job job2 = new job1();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
     }
 }
